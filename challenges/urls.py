0a3ce5d06f49e32814c4c3b02830315059ab5034
Robert Murak <robert.murak@gmail.com>
2023-05-30 21:04:15 +0200

    z domu

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 # przechwytujemy dynamicznie url'a i przekazujemy go do funkcji monthly_challenge w views
-# w nawiasach ostrych osadzamy <identyfikator> ktory pozniej wykorzystamy w funkcji monthly_challenge
+# w nawiasach ostrych osadzamy <identyfikator> ktory pozniej wykorzystamy w funkcji w
 urlpatterns = [
     path("<month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 # drugi argument funkcji (month) musi miec dokladnie ta sama nazwe ktora zdefiniowalismy jako <identyfikator> w urls.py
+
+
 def monthly_challenge(request, month):
     challenge_text = None
     if month == "january":
@@ -11,8 +13,8 @@ def monthly_challenge(request, month):
     elif month == "february":
         challenge_text = "February challenge"
     elif month == "march":
-        challenge_text = "March challenge" 
-    else:
-        return HttpResponseNotFound("Nie znaleziono takiej strony") 
-    return HttpResponse(challenge_text)
 
+        challenge_text = "March challenge"
+    else:
+        return HttpResponseNotFound("Nie znaleziono takiej strony")
+    return HttpResponse(challenge_text)
