50f5df9cb22c239092f7383e07eb13bda3719f56
Robert Murak <robert.murak@gmail.com>
2023-05-30 08:34:58 +0200

    add new months

--- challenges/views.py
+++ challenges/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index (request):
-    return HttpResponse("This works")
+def january (request):
+    return HttpResponse("January challenge")
+def february (request):
+    return HttpResponse("February challenge")
+def march (request):
+    return HttpResponse("March challenge")
+
